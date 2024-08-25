cf7a9b347a87b46745c02daf524419afa28feaeb
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2024-08-25 15:02:04 -0400

    Add Product Detail page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,11 +12,13 @@ import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App.tsx'
 import HomeScreen from './screens/HomeScreen.tsx';
+import ProductScreen from './screens/ProductScreen.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
       <Route path='/' element={<App />}>
         <Route index={true} path='/' element={<HomeScreen />} />
+        <Route index={true} path='/product/:id' element={<ProductScreen />} />
       </Route>
     )
   )
