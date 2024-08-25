83504bd2b842e595058075042dc905fea7584ba8
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2024-08-25 14:04:38 -0400

    Complete Router Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import HomeScreen from "./screens/HomeScreen"
 
 function App() {
 
@@ -11,7 +11,7 @@ function App() {
     <main>
       <Container>
         <h1>Welcome to the MapleMart</h1>
-        <HomeScreen />
+        <Outlet />
       </Container>
     </main>
     <Footer />
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,28 @@
-import { BrowserRouter } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App.tsx'
+import HomeScreen from './screens/HomeScreen.tsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <Route path='/' element={<App />}>
+        <Route index={true} path='/' element={<HomeScreen />} />
+      </Route>
+    )
+  )
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  </BrowserRouter>
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 )
